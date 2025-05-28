03502f341f75135cb0b14f5628989d14b13b837f
brunomoreschi <41950814+brunomoreschi@users.noreply.github.com>
2025-05-28 11:01:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ let storyWords = [];
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
-const word = input.value;  // Allow space
-    if (word) {
-        storyWords.push(word);
-storyDiv.textContent = storyWords.join(" ");
-        input.value = '';
+
+    const char = input.value;  // DO NOT trim!
+
+    if (char !== '') {  // Allow space but prevent empty input
+        storyWords.push(char);
+        storyDiv.textContent = storyWords.join('');
     }
+
+    input.value = '';  // Clear input after submission
 });
